02d39571a2719972f6d281aca8f1d45df38865d0
naretayami <naretayami29@gmail.com>
2021-11-08 15:08:56 +0900

    change

--- awayFromPicto.py
+++ awayFromPicto.py
@@ -1,5 +1,6 @@
 import pgzrun
 import random
+from random import randint
 
 WIDTH = 800
 HEIGHT = 800
@@ -7,21 +8,20 @@ TITLE = "A Big Adventure"
 time = 0
 point = 0
 
-# プレイヤークラス（Actorクラスを継承）
+
 class House(Actor):
     def __init__(self):
-        super().__init__("humanpicto")
+        super().__init__("house")
         self.pos = 400, 700
 
 class Human(Actor):
     def __init__(self):
-        super().__init__("humanpicto")
+        super().__init__("human")
         self.pos = 400, 600
-    
-# エネミークラス（Actorクラスを継承）
+
 class Enemy(Actor):
     def __init__(self):
-        super().__init__("humanpicto")
+        super().__init__("zombie")
         self.pos = random.randrange(800), random.randrange(800)
         self.dx = random.randrange(21) - 10
         self.dy = random.randrange(21) - 10
@@ -35,12 +35,13 @@ class Enemy(Actor):
 
 class Obstacle(Actor):
     def __init__(self):
-        super().__init__("humanpicto")
+        super().__init__("robot_down")
         self.pos = 400, 200
+    
 
 class Obstacle2(Actor):
     def __init__(self):
-        super().__init__("humanpicto")
+        super().__init__("robot_fall")
         self.pos = 100, 300
 
 ob1 = Obstacle()
@@ -50,14 +51,15 @@ house = House()
 enemys = [Enemy() for _ in range(5)]
 istouch = False
 
-# RED = 200, 0, 0
-# BOX = Rect((20, 20), (100, 100))
+def place_obstracle():
+    ob1.x = randint(20, (WIDTH - 20))
+    ob1.y = randint(20, (HEIGHT - 20))
+    ob2.x = randint(30, (WIDTH - 20))
+    ob2.y = randint(30, (HEIGHT - 20))
 
-# def on_mouse_move(pos):
-#     player.pos = pos
 
 def draw():
-    global time, point
+    global time, point, istouch
     screen.fill([255,255,255])
     screen.draw.text("Let's leave the house and on on a big big adventure!!!!!", (10, 10),color="orange")
     screen.draw.text("Time:" + str(time), (10,30), color="black") 
@@ -68,16 +70,33 @@ def draw():
     ob2.draw()
     for enemy in enemys:
         enemy.draw()
-    # screen.draw.circle((400, 400), 30, 'yellow')
-    # screen.draw.text("hello world", (20, 100),color="orange")
+    if istouch == True:
+        screen.fill([0,0,0])
+        screen.draw.text("Game Over", (220, 300),color="orange",fontsize=100)
+        screen.draw.text("Time:" + str(time), (320, 380),color="white",fontsize=70)
+        screen.draw.text("Number of people who set out on a big adventure is ...", (40, 450),color="white",fontsize=40)
+        screen.draw.text(str(point) + " people", (330, 480),color="white",fontsize=50)
+
     
-       
 def update():
+    global istouch, time
+    if istouch == True:
+        return
+    time = time + 1
+    for enemy in enemys:
+        enemy.update()
+        if abs(human.x - enemy.x) <= human.width - 40 and abs(human.y - enemy.y) <= human.height - 40:
+            istouch = True
+        if abs(human.x - ob1.x) <= human.width - 40 and abs(human.y - ob1.y) <= human.height - 40:
+            istouch = True
+        if abs(human.x - ob2.x) <= human.width - 40 and abs(human.y - ob2.y) <= human.height - 40:
+            istouch = True
     if human.y == 0:
         human.x = house.x
         human.y = house.y - 100
         global point
         point = point + 1
+        place_obstracle()
     if keyboard.left:
         house.x -= 2
         human.x -= 2
@@ -86,22 +105,6 @@ def update():
         human.x += 2
     if keyboard.space:
         human.y -= 2
-
-    global istouch, time
-    if istouch == True:
-        return
-    time = time + 1
-    for enemy in enemys:
-        enemy.update()
-        if abs(human.x - enemy.x) <= human.width - 5 and abs(human.y - enemy.y) <= human.height - 10:
-            istouch = True
-        if abs(human.x - ob1.x) <= human.width and abs(human.y - ob1.y) <= human.height:
-            istouch = True
-        if abs(human.x - ob2.x) <= human.width and abs(human.y - ob2.y) <= human.height:
-            istouch = True
+            
 
 pgzrun.go()
-
-
-# 画像を変える
-# 位置を変える
